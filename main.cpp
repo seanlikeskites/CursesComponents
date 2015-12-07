750f0c8a956ca7084476731cc1d5a8c1919bb292
Sean Enderby <sean.enderby@gmail.com>
2015-12-07 07:42:43 +0000

    putting it through its paces

--- main.cpp
+++ main.cpp
@@ -2,15 +2,46 @@
 
 int main()
 {
-    Slider slider ("Test");
-    slider.setBounds (2, 2, 6, 40);
-    slider.setRange (2, 80, 0.2);
+    const int numSliders = 4;
+    Slider sliders [numSliders] = {{"Ham"}, {"Jam"}, {"Hats"}, {"Cake"}};
+
+    int sliderWidth = 5;
+    int sliderHeights [numSliders] = {30, 26, 35, 19};
+    int sliderY = 2;
+    int sliderX = 2;
+
+    double sliderBottoms [numSliders] = {-4.0, 50.0, 37.6, 112.0};
+    double sliderTops [numSliders] = {5.0, -12.0, 34.2, 2000.34};
+    double sliderSkews [numSliders] = {1.0, 0.9, 12, 0.1};
+
+    for (int s = 0; s < numSliders; ++s)
+    {
+        sliders [s].setBounds (sliderX, sliderY, sliderWidth, sliderHeights [s]);
+        sliders [s].setRange (sliderBottoms [s], sliderTops [s], sliderSkews [s]);
+        sliderX += sliderWidth;
+    }
 
     int key;
+    int sliderIndex = 0;
 
     while ((key = getch()) != '\n')
     {
-        slider.keyPressed (key);
+        switch (key)
+        {
+            case KEY_RIGHT:
+                sliderIndex++;
+                sliderIndex %= numSliders;
+                break;
+
+            case KEY_LEFT:
+                sliderIndex--;
+                sliderIndex = (sliderIndex + numSliders) % numSliders;
+                break;
+
+            default:
+                sliders [sliderIndex].keyPressed (key);
+                break;
+        }
     }
 
     return 0;
