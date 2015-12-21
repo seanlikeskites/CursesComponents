5b7f4950fe77f1f9d064de93c7ffa1e6301f211a
Sean Enderby <sean.enderby@gmail.com>
2015-12-21 13:15:03 +0000

    timer doc update

--- Timer.hpp
+++ Timer.hpp
@@ -8,7 +8,7 @@
 
 /** A timer class which calls a given function periodically.
  *
- *  Classes which inherit this class will have their implementation timerCallback called
+ *  Classes which inherit this class will have their implementation timerCallback() called
  *  periodically.
  */
 class Timer
@@ -21,7 +21,7 @@ public:
 
     /** Start the timer.
      *
-     *  Starts the timer such that timerCallback will be called periodically using the
+     *  Starts the timer such that timerCallback() will be called periodically using the
      *  provided period.
      *
      *  @param newCallbackPeriod the new callback period
@@ -31,7 +31,7 @@ public:
     void startTimer (const std::chrono::milliseconds &newCallbackPeriod);
     /** Pause the timer.
      *
-     *  Causes the timer thread to busy sleep. The timerCallback will no longer get called
+     *  Causes the timer thread to busy sleep. The timerCallback() will no longer get called
      *  by the thread will continue to run in the background.
      */
     void pauseTimer();
