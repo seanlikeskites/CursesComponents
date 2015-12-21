5973124d6e345506e1b43bdae64bc4bb3bd0e089
Sean Enderby <sean.enderby@gmail.com>
2015-12-21 13:52:45 +0000

    doc updates and fixes to ranged stuff

--- MathsTools.hpp
+++ MathsTools.hpp
@@ -1,25 +1,38 @@
 #ifndef MATHS_TOOLS_HPP_INCLUDED
 #define MATHS_TOOLS_HPP_INCLUDED
 
+/** Some useful maths functions. */
 struct MathsTools
 {
+    /** A generic implementation of the sign function.
+     *
+     *  @param value the input value
+     *
+     *  Returns -1 for a negative input, 0 for a 0 input and 1 for a positive input.
+     */
     template <typename T>
     static int sign (T value)
     {
         return sign (value, std::is_signed <T>());
     }
 
+    /** Constrict a value to a given range.
+     *
+     *  @param value the input value
+     *  @param bottomValue one of the range boundaries
+     *  @param topValue the other range boundary
+     */
     template <typename T>
     static T constrictValueToRange (T value, T bottomValue, T topValue)
     {
         T maxValue = std::max (topValue, bottomValue);
         T minValue = std::min (topValue, bottomValue);
 
-        if (value > maxValue)
+        if (value >= maxValue)
         {
             return maxValue;
         }
-        else if (value < minValue)
+        else if (value <= minValue)
         {
             return minValue;
         }
--- RangedValue.hpp
+++ RangedValue.hpp
@@ -3,10 +3,17 @@
 
 #include <algorithm>
 
+/** A class which represents a value constricted to a certain range. */
 template <typename T>
 class RangedValue
 {
 public:
+    /** Constructor
+     *
+     *  @param valueInit the initial value
+     *  @param bottomValueInit one of the range boundaries
+     *  @param topValueInit the other range boundary
+     */
     RangedValue (T valueInit, T bottomValueInit, T topValueInit)
         : value (0),
           minValue (std::min (bottomValueInit, topValueInit)),
@@ -17,17 +24,24 @@ public:
         operator= (valueInit);
     }
 
+    /** Copy Constructor */
     RangedValue (const RangedValue &other) = default;
+    /** Copy Assignment Operator */
     RangedValue& operator= (const RangedValue &rhs) = default;
+    /** Destructor */
     ~RangedValue() = default;
 
+    /** Set the value. 
+     *
+     *  @param rhs the new value
+     */
     RangedValue& operator= (T rhs)
     {
-        if (rhs < minValue)
+        if (rhs <= minValue)
         {
             value = minValue;
         }
-        else if (rhs > maxValue)
+        else if (rhs >= maxValue)
         {
             value = maxValue;
         }
@@ -39,35 +53,57 @@ public:
         return *this;
     }
 
+    /** Implicit cast to the underlying type. */
     operator T() const
     {
         return value;
     }
 
+    /** Increment the value. 
+     *
+     *  @param rhs the value to increment by
+     */
     RangedValue& operator+= (const T &rhs)
     {
         operator= (value + rhs);
         return *this;
     }
 
+    /** Decrement the value. 
+     *
+     *  @param rhs the value to decrement by
+     */
     RangedValue& operator-= (const T &rhs)
     {
         operator= (value - rhs);
         return *this;
     }
 
+    /** Compound multiplication assignment.
+     *
+     *  @param rhs the value to multiply by
+     */
     RangedValue& operator*= (const T &rhs)
     {
         operator= (value * rhs);
         return *this;
     }
 
+    /** Compound division assignment.
+     *
+     *  @param rhs the value to divide by
+     */
     RangedValue& operator/= (const T &rhs)
     {
         operator= (value / rhs);
         return *this;
     }
 
+    /** Set the range.
+     *
+     *  @param newBottomValue one of the new range boundaries
+     *  @param newTopValue the other new range boundary
+     */
     void setRange (T newBottomValue, T newTopValue)
     {
           minValue = std::min (newBottomValue, newTopValue);
@@ -76,26 +112,31 @@ public:
           bottomValue = newBottomValue;
     }
 
+    /** Returns the range. */
     T getRange() const
     {
         return topValue - bottomValue;
     }
 
+    /** Returns the lowest end of the range. */
     T getMinValue() const
     {
         return minValue;
     }
 
+    /** Returns the highest end of the range. */
     T getMaxValue() const
     {
         return maxValue;
     }
 
+    /** Returns the bottom end of the range. */
     T getBottomValue() const
     {
         return bottomValue;
     }
 
+    /** Returns the top end of the range. */
     T getTopValue() const
     {
         return topValue;
